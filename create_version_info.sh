5e64b366606f89befbb081ce7c635a62cf7403d5
Sebastien ROBERT <sebastien.saigo@gmail.com>
2017-11-21 23:50:38 +0900

    Fixed version script to copy git info to asset folder too

--- create_version_info.sh
+++ create_version_info.sh
@@ -4,3 +4,4 @@ GIT_COMMIT=`git rev-parse HEAD`
 GIT_REMOTE_ORIGIN_URL=`git config --get remote.origin.url`
 
 echo -e "{\"gitBranch\": \"${GIT_BRANCH}\",\"gitCommit\": \"${GIT_COMMIT}\",\"gitRemoteOriginUrl\": \"${GIT_REMOTE_ORIGIN_URL}\"}" > src/assets/git-info.json
+cp src/assets/git-info.json docs/assets/git-info.json
