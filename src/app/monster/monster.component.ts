80f6b5d9bb95b69d892cb45a79260b8c39fcdfcb
Sebastien ROBERT <sebastien.saigo@gmail.com>
2018-04-22 01:15:50 +0900

    Changed elemental and ailments display (now two columns, made them smaller to fit better vertically)

--- src/app/monster/monster.component.ts
+++ src/app/monster/monster.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { LanguageService } from '../language.service';
 import { IMonsterInfoViewModel } from '../monster-info/monster-info.component';
 import { GlobalsService } from '../globals.service';
+import { IAttributeInfo, Attribute } from '../data-structures/attribute-info';
 
 @Component({
     selector: 'app-monster',
@@ -17,6 +18,26 @@ export class MonsterComponent implements OnInit {
         public languageService: LanguageService,
         public globalsService: GlobalsService
     ) {
+    }
+
+    private isElemental(attr: IAttributeInfo): boolean {
+        switch (attr.type) {
+            case Attribute.Fire:
+            case Attribute.Thunder:
+            case Attribute.Water:
+            case Attribute.Ice:
+            case Attribute.Dragon:
+                return true;
+        }
+        return false;
+    }
+
+    public filterElemental(items: IAttributeInfo[]): IAttributeInfo[] {
+        return items.filter(this.isElemental);
+    }
+
+    public filterNonElemental(items: IAttributeInfo[]): IAttributeInfo[] {
+        return items.filter(x => !this.isElemental(x));
     }
 
     ngOnInit() {
