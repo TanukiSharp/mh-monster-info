d3e3a29d15b0793dadd2b97b24f00c9eb8c076c7
Sebastien ROBERT <sebastien.saigo@gmail.com>
2019-09-24 23:17:34 +0900

    Merged make_publishable.sh and create_version_info.sh scripts, added git commands in between

--- create_version_info.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`
-GIT_COMMIT=`git rev-parse HEAD`
-GIT_REMOTE_ORIGIN_URL=`git config --get remote.origin.url`
-
-mkdir -p docs/assets
-
-echo -e "{\"gitBranch\": \"${GIT_BRANCH}\",\"gitCommit\": \"${GIT_COMMIT}\",\"gitRemoteOriginUrl\": \"${GIT_REMOTE_ORIGIN_URL}\"}" > src/assets/git-info.json
-cp src/assets/git-info.json docs/assets/git-info.json
--- make_publishable.sh
+++ make_publishable.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
+
+# Build Angular stuffs
 rm -rf docs
 ng build --prod --base-href "/mh-monster-info/" --output-path docs
+
+# Commit newly generated assets
+git add docs/
+git commit -m "Updated github pages" -n
+
+# Generate the git info
+GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+GIT_COMMIT=`git rev-parse HEAD`
+GIT_REMOTE_ORIGIN_URL=`git config --get remote.origin.url`
+
+echo -e "{\"gitBranch\": \"${GIT_BRANCH}\",\"gitCommit\": \"${GIT_COMMIT}\",\"gitRemoteOriginUrl\": \"${GIT_REMOTE_ORIGIN_URL}\"}" > src/assets/git-info.json
+cp src/assets/git-info.json docs/assets/git-info.json
+
+# Commit the git info
+git add docs/assets/git-info.json
+git commit -m "Updated git version info" -n
